772d8424ea2f09e7db3f8fbaf56b4d54d258ac56
Adam Angle <AdamAngle@users.noreply.github.com>
2024-09-17 05:34:54 +0000

    js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,4 @@
-const email = "VUVkRloyRklTbXhhYWpCcFlsZEdjR0pJVW5aUGJVWnJXVmN4UVZsWE5XNWlSMVYxV2tkV01rbHFOV2hhUjBaMFVVZEdkVm95ZUd4TWJWSnNaR3AzZGxsVU5EMD0=";
+const email = "VUVkRloyRklTbXhhYWpCcFlsZEdjR0pJVW5aUGJYQm9Zekl4Y0dKdFZrRlpWelZ1WWtkVmRWcEhWakpKYWpWeFdWaE9kR0ZYTld4UlIwWjFXako0YkV4dFVteGthbmQyV1ZRMFBRPT0=";
 document.getElementById('show-email').addEventListener('click',function(event) {
   event.preventDefault();
   document.getElementById('email-contents').innerHTML = atob(atob(atob(email)));
